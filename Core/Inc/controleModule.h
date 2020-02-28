d2e975077fd98a5f4c345962f18efc6261bca767
Deuter <wojciechbernat95@gmail.com>
2020-02-28 22:35:44 +0100

    Brief of achievements and added prototypes of payload's functions

--- Core/Inc/controleModule.h
+++ Core/Inc/controleModule.h
@@ -24,8 +24,8 @@ void setBit(uint8_t addr, bitNum_t bit);
 void resetBit(uint8_t addr, bitNum_t bit);
 
 /* Multi bytes read/write register functions */
-void multiRead(uint8_t addr, uint8_t *buf, size_t bufSize);  //TODO: to test
-void multiWrite(uint8_t addr, uint8_t *buf, size_t bufSize); //TODO: to test
+void multiRead(uint8_t addr, uint8_t *buf, size_t bufSize);
+void multiWrite(uint8_t addr, uint8_t *buf, size_t bufSize);
 
 /* Turn on and turn off module */
 void powerUp(); //Fixed
@@ -33,3 +33,86 @@ void powerDown();
 
 /* Additional feature functions */
 uint8_t getStatus();
+
+
+/*
+ * TX and RX payloads access functions
+ */
+
+/*
+ * @Brief	Read received payload
+ * @Param	Pointer to payload's buffer
+ * @Param	Buffer size
+ * @Retval	Return status of execution
+ * 			OK_CODE if arguments are correct or function execute correct
+ * 			ERR_CODE if arguments are incorrect or function return error of execution
+ */
+uint8_t readRxPayload(uint8_t *buf, size_t bufSize);
+
+/*
+ * @Brief	Write  payload to transmit
+ * @Param	Pointer with payload's buffer
+ * @Param	Buffer size
+ * @Retval	Return status of execution
+ * 			OK_CODE if arguments are correct or function execute correct
+ * 			ERR_CODE if arguments are incorrect or function return error of execution
+ */
+uint8_t writeTxPayload(uint8_t *buf, size_t bufSize);
+
+/*
+ * @Brief	Read received payload width for the top
+ * @Param	Pointer to payload's buffer
+ * @Param	Buffer size
+ * @Param 	Width of bytes to read
+ * @Retval	Return status of execution
+ * 			OK_CODE if arguments are correct or function execute correct
+ * 			ERR_CODE if arguments are incorrect or function return error of execution
+ */
+uint8_t readRxPayloadWidth(uint8_t *buf, size_t bufSize, uint8_t width);
+
+/*
+ * @Brief	Write  payload to transmit together wich ACK packet
+ * @Param	Pointer with payload's buffer
+ * @Param	Buffer size
+ * @Retval	Return status of execution
+ * 			OK_CODE if arguments are correct or function execute correct
+ * 			ERR_CODE if arguments are incorrect or function return error of execution
+ */
+uint8_t writeTxPayloadAck(uint8_t *buf, size_t bufSize);
+
+/*
+ * @Brief	Write payload to transmit without ACK packet on this specify
+ * @Param	Pointer with payload's buffer
+ * @Param	Buffer size
+ * @Retval	Return status of execution
+ * 			OK_CODE if arguments are correct or function execute correct
+ * 			ERR_CODE if arguments are incorrect or function return error of execution
+ */
+uint8_t writeTxPayloadNoAck(uint8_t *buf, size_t bufSize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
