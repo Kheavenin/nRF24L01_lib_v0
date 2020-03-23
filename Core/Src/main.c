d37c08fb52b35483ba3982dcc74fee5d3078069c
Deuter <wojciechbernat95@gmail.com>
2020-03-23 10:28:33 +0100

    Added UART2 to PC communication

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,6 +22,7 @@
 #include "main.h"
 #include "spi.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -118,6 +119,7 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_TIM1_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 	HAL_TIM_Base_Start(&htim1);
   /* USER CODE END 2 */
